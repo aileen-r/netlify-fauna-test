ad54b21691a28075b69aea1d309b8e1b3d56ba18
Aileen Rae <aileen@stormideas.com>
2020-06-21 17:33:02 +0100

    Add eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+  },
+  globals: {
+    $nuxt: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};
