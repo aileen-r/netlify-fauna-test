e415eafd97c88837bf1e4c92519d2b736f1a2b55
Aileen Rae <aileen@stormideas.com>
2020-06-21 18:30:04 +0100

    Copy over files from template vue + fauna + netlify tutorial

--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import attemptToAuthoriseTokens from './helpers/authorise-tokens';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
+
+store.dispatch('auth/initAuth');
+
+attemptToAuthoriseTokens();
+
+Vue.config.productionTip = false;
+
+// Register a global custom directive called `v-focus`
+Vue.directive('focus', {
+  // When the bound element is inserted into the DOM...
+  inserted: function (el) {
+    // Focus the element
+    el.focus();
+  },
+});
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,59 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import store from '../store';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    component: () => import('../views/Home'),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: '/journals',
+    name: 'journals',
+    component: () => import('../views/AllJournals'),
+    meta: { authRequired: true },
+  },
+  {
+    path: '/journals/:id/posts',
+    name: 'posts',
+    component: () => import('../views/AllPosts'),
+    meta: { authRequired: true },
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import('../views/Profile'),
+    meta: { authRequired: true },
+  },
+  {
+    path: '/recover',
+    name: 'recover',
+    component: () => import('../views/RecoverAccount'),
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const authRequired = to.matched.some((route) => route.meta.authRequired);
+  // If the route doesnt have a `meta.authRequired` property go on ahead!
+  if (!authRequired) {
+    return next();
+  }
+  // If we go this far then it must have the `meta.authRequired`. But is there is a user logged in? If so, then go right on ahead!
+  if (store.getters['auth/loggedIn']) {
+    return next();
+  }
+  // The page requested is both secured and there is no logged in user detected. Sorry mate. No entry!
+  console.warn('Page restricted, you need to login');
+  next({ name: 'home', query: { redirectFrom: to.fullPath } });
+});
+
+export default router;
