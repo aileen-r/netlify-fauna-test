555a515194f490a93c788828562b2af808f247c8
Aileen Rae <aileen@stormideas.com>
2020-07-04 15:55:10 +0100

    Add file uploader with file type validation

--- src/views/UserSettings.vue
+++ src/views/UserSettings.vue
@@ -1,31 +1,78 @@
 <template>
-  <h2>User Settings</h2>
+  <div>
+    <h2>User Settings</h2>
+    <b-form novalidate @submit="updateUser">
+      <b-form-group label="Profile Picture" label-for="profile-picture">
+        <b-form-file
+          id="profile-picture"
+          v-model="imageFile"
+          :state="isFileValid"
+          accept="image/*"
+          placeholder="Choose a file or drop it here..."
+          drop-placeholder="Drop file here..."
+        />
+        <b-form-invalid-feedback :state="isFileValid">
+          Please select an image file type.
+        </b-form-invalid-feedback>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Save Changes</b-button>
+    </b-form>
+  </div>
 </template>
 
 <script>
 import s3, { bucketName, bucketRegion } from '@/helpers/init-aws';
 export default {
   name: 'UserSettings',
+
   data() {
     return {
+      imageFile: null,
       photos: [],
     };
   },
 
+  computed: {
+    isFileValid() {
+      if (this.imageFile) {
+        return this.imageFile.type.startsWith('image/');
+      } else {
+        return null;
+      }
+    },
+  },
+
   mounted() {
-    s3.listObjects({ Delimiter: '/' })
-      .promise()
-      .then((data) => {
-        const bucketUrl = `https://${bucketName}.s3.${bucketRegion}.amazonaws.com/`;
-        this.photos = data.Contents.map((photo) => {
-          const photoKey = photo.Key;
-          const photoUrl = bucketUrl + encodeURIComponent(photoKey);
-          return photoUrl;
+    this.getAllBucketObjects();
+  },
+
+  methods: {
+    getAllBucketObjects() {
+      s3.listObjects({ Delimiter: '/' })
+        .promise()
+        .then((data) => {
+          const bucketUrl = `https://${bucketName}.s3.${bucketRegion}.amazonaws.com/`;
+          this.photos = data.Contents.map((photo) => {
+            const photoKey = photo.Key;
+            const photoUrl = bucketUrl + encodeURIComponent(photoKey);
+            return photoUrl;
+          });
+        })
+        .catch((err) => {
+          console.error('Error fetching s3 objects.', err.message);
         });
-      })
-      .catch((err) => {
-        console.error('Error fetching s3 objects.', err.message);
-      });
+    },
+
+    updateUser(e) {
+      e.preventDefault();
+      if (this.imageFile) {
+        this.uploadFileToS3(this.imageFile);
+      }
+    },
+
+    uploadFileToS3(file) {
+      console.log(file);
+    },
   },
 };
 </script>
